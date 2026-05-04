f2d69e3adb9b92caab1ecb194914ee80f9921fef
tsuki-dev001 <c.heloiza@escola.pr.gov.br>
2026-05-04 11:56:13 -0300

    .

--- main.js
+++ main.js
@@ -2,14 +2,14 @@ const botoes = document.querySelectorAll("botoes")
 console.log(botoes)
 
 for (let i = 0; i < botoes.length; i++) {
-    botoes[i].onclick = function (){
+  botoes[i].onclick = function () {
 
-      for(let j=0; j<botoes.length; j++){
-        botoes[j].classList.remove("ativo");
-      }
-
-
-
-        botoes[i].classList.add("ativo");
+    for (let j = 0; j < botoes.length; j++) {
+      botoes[j].classList.remove("ativo");
+      textos[j].classList.remove("ativo");
     }
+
+    botoes[i].classList.add("ativo");
+    textos[i].classList.add("ativo");
+  }
 }
